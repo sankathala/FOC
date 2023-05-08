dc18dd6368f605b4819f32880973dd3985953ffe
sankathala <130464046+sankathala@users.noreply.github.com>
2023-05-08 12:44:35 +0530

    Add files via upload

---
+++ decimal to hexadecimal foc.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+  
+int main()
+{
+    int decimalNumber = 45;
+    
+    // printing hexadecimal number
+    // using format specifier %X
+    printf("Hexadecimal number is: %X", decimalNumber);
+    return 0;
+}
