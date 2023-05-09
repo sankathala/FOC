5daa697c31d36857c68ff358f1b890b8d6acf491
sankathala <130464046+sankathala@users.noreply.github.com>
2023-05-09 12:34:55 +0530

    Add files via upload

---
+++ summing up Nn foc.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+  
+// Function to calculate the following series
+int Series(int n)
+{
+    int i;
+    int sums = 0;
+    for (i = 1; i <= n; i++)
+        sums += (i * i);
+    return sums;
+}
+  
+// Driver Code
+int main()
+{
+    int n = 3;
+    int res = Series(n);
+    printf("%d", res);
+}
